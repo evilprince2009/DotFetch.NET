c063933755d30ec30ca241eacbef61f3d4df5774
Ibne Nahian <evilprince2009@gmail.com>
2021-09-22 22:17:37 +0600

    ascii art

--- DotFetch.NET/LogoRenderer.cs
+++ DotFetch.NET/LogoRenderer.cs
@@ -2,6 +2,41 @@ namespace DotFetch.NET
 {
     public class LogoRenderer
     {
-        
+        public static List<string> Windows()
+		{
+			List<string> LogoBuffer = new List<string>()
+			{
+				{ "                                       ```...--://+oo`" },
+				{ "                         ```...--://+oooossyyyyyyyyyy`" },
+				{ "           ```...--:/+- .osssyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ " ..--:/++ooosssyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "`yyyyyyyyyyyyyyyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ " ..--:/++ooosssyyyyyyy: -yyyyyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "           ```...--:/+- .oossyyyyyyyyyyyyyyyyyyyyyyyy`" },
+				{ "                         ```...--:/++ooosssyyyyyyyyyy`" },
+				{ "                                       ```...--::/+oo`" }
+
+			};
+
+			return LogoBuffer;
+		}
     }
 }
