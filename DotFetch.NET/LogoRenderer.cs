0d4dfde717337d605ab5b41db2ec47ec00f2a24f
Ibne Nahian <evilprince2009@gmail.com>
2021-09-23 16:09:25 +0600

    CrossSkull

--- DotFetch.NET/LogoRenderer.cs
+++ DotFetch.NET/LogoRenderer.cs
@@ -20,7 +20,36 @@ namespace DotFetch.NET
                 logo = DefaultDevil();
             }
 
-            return logo;
+            return CrossSkull();
+        }
+
+        private static List<string> CrossSkull()
+        {
+            List<string> AsciiArt = new()
+            {
+                { "NO!                          MNO!          " },
+                { "     MNO!!         [NBK]          MNNOO!   " },
+                { "   MMNO!                           MNNOO!! " },
+                { " MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!!  " },
+                { " !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO!      " },
+                { "       ! MMMMMMMMMMMMMPPPPOOOOIII! !       " },
+                { "        MMMMMMMMMMMMPPPPPOOOOOOII!!        " },
+                { "        MMMMMOOOOOOPPPPPPPPOOOOMII!        " },
+                { "        MMMMM..    OPPMMP    .,OMI!        " },
+                { "        MMMM::   o.,OPMP,.o   ::I!!        " },
+                { "          NNM:::.,,OOPM!P,.::::!!          " },
+                { "         MMNNNNNOOOOPMO!!IIPPO!!O!         " },
+                { "         MMMMMNNNNOO:!!:!!IPPPPOO!         " },
+                { "          MMMMMNNOOMMNNIIIPPPOO!!          " },
+                { "             MMMONNMMNNNIIIOO!             " },
+                { "           MN MOMMMNNNIIIIIO! OO           " },
+                { "          MNO! IiiiiiiiiiiiI OOOO          " },
+                { "     NNN.MNO!   O!!!!!!!!!O   OONO NO!     " },
+                { "    MNNNNNO!    OOOOOOOOOOO    MMNNON!     " },
+                { "      MNNNNO!    PPPPPPPPP    MMNON!       " },
+                { "         OO!                   ON!         " }
+            };
+            return AsciiArt;
         }
 
         private static List<string> DefaultDevil()
