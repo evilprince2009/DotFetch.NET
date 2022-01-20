e46610131fedf52be9945f826e8dc13a77f4d0b2
Ibne Nahian <evilprince2009@gmail.com>
2022-01-20 18:43:21 +0600

    Update ProcessExtensions.cs

--- DotFetch.NET/Implementation/ProcessExtensions.cs
+++ DotFetch.NET/Implementation/ProcessExtensions.cs
@@ -8,26 +8,26 @@ namespace DotFetch.NET.Implementation
         {
             var processName = Process.GetProcessById(pid).ProcessName;
             var processesByName = Process.GetProcessesByName(processName);
-            string processIndexdName = null;
+            string? processIndexdName = null;
             
             for (var index = 0; index < processesByName.Length; index++)
             {
                 processIndexdName = index == 0 ? processName : processName + "#" + index;
-                PerformanceCounter processId = new ("Process", "ID Process", processIndexdName);
+                PerformanceCounter processId = new("Process", "ID Process", processIndexdName);
                 
                 if ((int) processId.NextValue() == pid)
                 {
                     return processIndexdName;
                 }
             }
-            
+
             return processIndexdName;
         }
 
     private static Process FindPidFromIndexedProcessName(string indexedProcessName)
     {
-        PerformanceCounter parentId = new ("Process", "Creating Process ID", indexedProcessName);
-        return Process.GetProcessById((int) parentId.NextValue());
+            PerformanceCounter parentId = new("Process", "Creating Process ID", indexedProcessName);
+            return Process.GetProcessById((int)parentId.NextValue());
     }
 
     public static Process Parent(this Process process) => FindPidFromIndexedProcessName(FindIndexedProcessName(process.Id));
